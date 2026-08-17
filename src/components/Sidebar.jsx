00fe585887dd396b7c092c8af8e9401869ba94a4
JINAL RAVAL <jinalraval2022@gmail.com>
2026-08-17 13:51:26 +0100

    "sidebar"

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,11 +17,11 @@ export default function Sidebar() {
           </li>
 
           <li onClick={() => navigate("/Favorites")}>
-          Favorites
+          Favorites ❤️ 
           </li>
 
           <li onClick={() => navigate("/Veg-nonveg")}>
-          Veg / Non-Veg
+          Veg 🌱 / Non-Veg 🥩
           </li>
       </ul>
     </aside>
